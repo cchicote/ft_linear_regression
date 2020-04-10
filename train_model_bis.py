fixed normalization for data, waiting for improvements for theta de-normalization

train_model_bis.py
@@ -11,10 +11,12 @@ class Dataset:
 	def __init__(self, tab_x, tab_y):
 		self.tab_x = np.array(tab_x, dtype=np.float64)
 		self.tab_y = np.array(tab_y, dtype=np.float64)
-		self.max_x = self.tab_x.max() 
+		self.min_x = self.tab_x.min()
+		self.max_x = self.tab_x.max()
+		self.min_y = self.tab_y.min() 
 		self.max_y = self.tab_y.max()
-		self.norm_tab_x = utils.normalize(self.tab_x, self.max_x)
-		self.norm_tab_y = utils.normalize(self.tab_y, self.max_y)
+		self.norm_tab_x = utils.normalize(self.tab_x, self.max_x, self.min_x)
+		self.norm_tab_y = utils.normalize(self.tab_y, self.max_y, self.min_y)
 		self.sorted_tab_x, self.sorted_tab_y = zip(*sorted(zip(self.tab_x, self.tab_y)))
 		self.theta0 = 0
 		self.norm_theta0 = 0
@@ -28,12 +30,13 @@ def print_dataset(data):
 
 def train_model(ratio, theta0, theta1, tab_x, tab_y):
 	m = len(tab_x)
-	tmp_t0, tmp_t1 = 0, 0
+	tmp_t0, tmp_t1, estimated_price = 0, 0, 0
 	for i in range(m):
-		tmp_t0 += ratio * (est.estimate_price(tab_x[i], theta0, theta1) - tab_y[i])
-		tmp_t1 += ratio * (est.estimate_price(tab_x[i], theta0, theta1) - tab_y[i]) * tab_x[i]
-	theta0 -= tmp_t0
-	theta1 -= tmp_t1
+		estimated_price = est.estimate_price(tab_x[i], theta0, theta1)
+		tmp_t0 += ratio * (estimated_price - tab_y[i])
+		tmp_t1 += ratio * (estimated_price - tab_y[i]) * tab_x[i]
+	theta0 -= tmp_t0 / m
+	theta1 -= tmp_t1 / m
 	return (theta0, theta1)
 
 def plot_estimations(data, labelx, labely):
@@ -81,6 +84,10 @@ def main():
 	# train model
 	for i in range(loops):
 		data.norm_theta0, data.norm_theta1 = train_model(ratio, data.norm_theta0, data.norm_theta1, data.norm_tab_x, data.norm_tab_y)
+	
+	not_norm_theta0, not_norm_theta1 = 0, 0
+	for i in range(loops):
+		not_norm_theta0, not_norm_theta1 = train_model(ratio, not_norm_theta0, not_norm_theta1, data.tab_x, data.tab_y)
 
 	# save thetas
 	data.theta0 = data.norm_theta0 * data.max_y